fe013712bb105df523eca0ec33f47a09d94eb7af
Alberto Castro Olea <alberto.reed@hotmail.es>
2024-04-07 19:48:07 +0200

    Replaced hotels and cities collections with characters one

--- server/config/hotel.middleware.js
+++ server/config/hotel.middleware.js
@@ -9,9 +9,5 @@ const hotelMiddleware = (req, res, next) => {
 };
 
 module.exports = (req, res, next) => {
-  if (req.path === '/hotels') {
-    hotelMiddleware(req, res, next);
-  } else {
-    next();
-  }
+  next();
 };
